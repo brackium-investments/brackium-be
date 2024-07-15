722e6aaac3791873a552efabc24a2f2b2a987259
ejim favour <favourejim56@gmail.com>
2024-07-15 06:55:21 +0100

    created a controller for protecting the investors route

--- controllers/investor/authController.js
+++ controllers/investor/authController.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
 const Investor = require('../../models/investorModel');
 const catchAsync = require('../../utils/catchAsync');
 const AppError = require('../../utils/appError');
@@ -78,6 +79,50 @@ const signInInvestor = catchAsync(async (req, res, next) => {
 // forgot password
 // reset password
 // protect
+// middleware to protect tours route
+const protect = catchAsync(async (req, res, next) => {
+  // 1) get the token and check if it exists
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  // 401- unauthorized
+  if (!token) {
+    return next(
+      new AppError(`You are not logged in! Please login to get access.`, 401),
+    );
+  }
+  // 2) validate (verify) the token
+  const decoded = await promisify(jwt.verify)(
+    token,
+    process.env.JWT_SECRET,
+    () => {},
+  );
+
+  // 3) check if the user still exists
+  const currentUser = await Investor.findById(decoded.id);
+
+  if (!currentUser) {
+    return next(
+      new AppError(`The user belonging to this token no longer exists`, 401),
+    );
+  }
+
+  // 4) check if user changed password after the JWT (token) was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError(`User recently changed password! Please login again`, 401),
+    );
+  }
+
+  // Grant access to protected route
+  req.investor = currentUser;
+  next();
+});
 
 module.exports = {
   createInvestor,
